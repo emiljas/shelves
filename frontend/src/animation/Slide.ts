f3dae752e5ea39428172336eac30ab338c889d61
Emil Jasiński <emiljasinski@gmail.com>
2015-12-01 23:17:44 +0100

    x

--- frontend/src/animation/Slide.ts
+++ frontend/src/animation/Slide.ts
@@ -2,34 +2,35 @@ import SlideArgs = require('./SlideArgs');
 
 const HALF_OF_PI = Math.PI / 2;
 
-interface Result { xMove: number, isDone: boolean };
+interface Result { xMove: number; isDone: boolean; }
 
 //full move per second
 class Slide {
-    private distance: number;
-    private distanceLeft: number;
-    private xMove: number;
-    private startXMove: number;
-    private startTimeStamp: number;
+    private DISTANCE: number;
+    private X_MOVE: number;
+    private TIMESTAMP: number;
 
     constructor(args: SlideArgs) {
-        this.distance = args.distance;
-        this.distanceLeft = args.distance;
-        this.xMove = args.xMove;
-        this.startXMove = args.xMove;
-        this.startTimeStamp = args.timestamp;
+        this.DISTANCE = args.distance;
+        this.X_MOVE = args.xMove;
+        this.TIMESTAMP = args.timestamp;
     }
 
     public frame(timestamp: number): Result {
-        let secsFromStart = (timestamp - this.startTimeStamp) / 1000;
-
-        let xMove = Math.round(this.startXMove + Math.sin(secsFromStart * HALF_OF_PI) * this.distance);
+        let secsFromStart = (timestamp - this.TIMESTAMP) / 1000;
 
         if (secsFromStart >= 1) {
-            return { xMove: this.startXMove + this.distance, isDone: true };
+            return {
+                xMove: this.X_MOVE + this.DISTANCE,
+                isDone: true
+            };
+        } else {
+            let xMove = this.X_MOVE + Math.sin(secsFromStart * HALF_OF_PI) * this.DISTANCE;
+            return {
+                xMove: xMove,
+                isDone: false
+            };
         }
-
-        return { xMove: xMove, isDone: false };
     }
 }
 
